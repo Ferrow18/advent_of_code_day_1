7eed13b6027a4a3efbd71ad416313cb66bd019ae
Ferrow <gerhat.fr@gmail.com>
2024-12-01 12:34:28 +0100

    add part two with two solutions and speed test

--- script.ts
+++ script.ts
@@ -41,9 +41,70 @@ const checkDistance = (firstList: number[], secondList: number[]) => {
 };
 
 const totalDistance = () => {
+  const startTime = Date.now();
   const [firstList, secondList] = formatList(inputs);
   sortLists(firstList, secondList);
   checkDistance(firstList, secondList);
+  const endTime = Date.now();
+  console.log("totalDistance time in ms: ", endTime - startTime);
 };
 
 totalDistance();
+
+// Part two, two solutions
+
+// first solution
+//avg 177ms
+
+// const similarityCheck = (firstList: number[], secondList: number[]) => {
+//   let count = {};
+//   for (let i = 0; i < firstList.length; i++) {
+//     let numberCounted = 0;
+//     for (let j = 0; j < secondList.length; j++) {
+//       if (firstList[i] === secondList[j]) {
+//         numberCounted = numberCounted + 1;
+//       }
+//     }
+//     count = { ...count, [firstList[i]]: firstList[i] * numberCounted };
+//   }
+//   const similarityScoreTotal = Object.values(count).reduce((a, b) => {
+//     if (typeof a !== "number" || typeof b !== "number") {
+//       throw new Error("a is not a number");
+//     }
+//     return a + b;
+//   }, 0);
+//   // console.log("counted: ", similarityScoreTotal);
+//   return similarityScoreTotal;
+// };
+
+// second solution
+// avg 9ms
+
+const similarityCheck = (firstList: number[], secondList: number[]) => {
+  const count = firstList.reduce((acc, num) => {
+    acc[num] =
+      (acc[num] || 0) + num * secondList.filter((n) => n === num).length;
+    // console.log("acc: ", acc);
+    return acc;
+  }, {});
+
+  const similarityScoreTotal = Object.values(count).reduce((a, b) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      throw new Error("a is not a number");
+    }
+    return a + b;
+  }, 0);
+  console.log("similarityScore: ", similarityScoreTotal);
+  return similarityScoreTotal;
+};
+
+const totalSimilarityScore = () => {
+  const startTime = Date.now();
+  const [firstList, secondList] = formatList(inputs);
+  sortLists(firstList, secondList);
+  similarityCheck(firstList, secondList);
+  const endTime = Date.now();
+  console.log("similarityScore time in ms: ", endTime - startTime);
+};
+
+totalSimilarityScore();
